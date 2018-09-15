22caf661edb53faeb2091e98fe3a1e5852372216
miquido-pawel-kocot <pawel.kocot@miquido.com>
2018-09-15 20:44:46 +0200

    test fixes

--- tests/TypedCollection/IntegerCollectionTest.php
+++ tests/TypedCollection/IntegerCollectionTest.php
@@ -1,18 +1,13 @@
 <?php
-
 declare(strict_types=1);
-
 namespace Tests\Miquido\DataStructure\TypedCollection;
-
 use Miquido\DataStructure\TypedCollection\IntegerCollection;
 use PHPUnit\Framework\TestCase;
-
 final class IntegerCollectionTest extends TestCase
 {
     public function testStaticCreation(): void
     {
         $integers = IntegerCollection::create(1, 2, 3);
-
         $this->assertCount(3, $integers);
         $this->assertSame([1, 2, 3], $integers->values());
         $this->assertSame([1, 2, 3], $integers->toArray());
@@ -20,47 +15,38 @@ final class IntegerCollectionTest extends TestCase
         $this->assertTrue($integers->includes(2));
         $this->assertTrue($integers->includes(3));
     }
-
     public function testIteratorWorks(): void
     {
         $integers = new IntegerCollection(1, 2, 3);
-
         $iterations = 0;
         foreach ($integers as $i) {
             ++$iterations;
             $this->assertContains($i, [1, 2, 3]);
         }
-
         $this->assertSame(3, $iterations);
     }
-
     public function testPush(): void
     {
         $integers = new IntegerCollection(1, 2, 3);
         $integers2 = $integers->push(4);
-
         $this->assertCount(3, $integers);
         $this->assertTrue($integers->includes(1));
         $this->assertTrue($integers->includes(2));
         $this->assertTrue($integers->includes(3));
-
         $this->assertCount(4, $integers2);
         $this->assertTrue($integers2->includes(1));
         $this->assertTrue($integers2->includes(2));
         $this->assertTrue($integers2->includes(3));
         $this->assertTrue($integers2->includes(4));
     }
-
     public function testUnique(): void
     {
         $integers = new IntegerCollection(1, 2, 3, 3, 1, 2);
         $unique = $integers->unique();
-
         $this->assertCount(6, $integers);
         $this->assertTrue($integers->includes(1));
         $this->assertTrue($integers->includes(2));
         $this->assertTrue($integers->includes(3));
-
         $this->assertCount(3, $unique);
         $this->assertTrue($unique->includes(1));
         $this->assertTrue($unique->includes(2));
